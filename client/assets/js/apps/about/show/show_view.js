65dfe643c025f65235db818e3cc40c608580f633
vinitm <my_name_is97@yahoo.com>
2016-05-31 18:08:15 +0530

    add client side caching to reduce ajax calls

--- client/assets/js/apps/about/show/show_view.js
+++ client/assets/js/apps/about/show/show_view.js
@@ -1,5 +1,5 @@
 var Marionette = require('backbone.marionette');
 module.exports = Marionette.ItemView.extend({
     template: require('./layout_template.tpl'),
-    className: 'container'
+    className: 'container row'
 });
--- client/assets/js/entities/details.js
+++ client/assets/js/entities/details.js
@@ -1,7 +1,32 @@
 var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
+var cache = require('../helpers/cache')(15);
 var medicineChannel = require('backbone.radio').channel('medicine');
 
+
+
+var Detail = Backbone.Model.extend({
+    fetch: function (options) {
+        options = options || {};
+        var success = options.success;
+        var value = cache.get(this.url);
+        if (value !== undefined) {
+            this.set(this.parse(value, options), options);
+            if (success)
+                success(this, value, options);
+
+            // Returns deferred as the original fetch
+            return Promise.resolve(value);
+        }
+
+
+        return Backbone.Model.prototype.fetch.apply(this, options)
+            .then(function (result) {
+                cache.add(this.url, result);
+                return result;
+            });
+    }
+});
 module.exports = Marionette.Object.extend({
     initialize: function () {
         var self = this;
@@ -10,7 +35,7 @@ module.exports = Marionette.Object.extend({
         });
     },
     getDetail: function (medicine) {
-        var detail = new Backbone.Model();
+        var detail = new Detail();
         detail.url = "/medicine_details/?id=" + encodeURIComponent(medicine);
         return detail.fetch().then(function () {
             return detail;
